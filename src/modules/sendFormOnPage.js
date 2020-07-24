c6cd952fb3d5c4d513e9399080bab3b0d16fb3f1
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-07-25 02:03:19 +0300

    Формы

--- src/modules/sendFormOnPage.js
+++ src/modules/sendFormOnPage.js
@@ -86,76 +86,76 @@ statusMessage.style.cssText = 'font-size: 2rem;';
 // });
 
 
-form2.addEventListener('submit', event => {
-    event.preventDefault();
-    // if (document.getElementById('check1').checked === true){
+// form2.addEventListener('submit', event => {
+//     event.preventDefault();
+//     // if (document.getElementById('check1').checked === true){
 
-        form2.appendChild(statusMessage);
+//         form2.appendChild(statusMessage);
 
-    const formData = new FormData(form2);
-    const body = {};
-    formData.forEach((val, key) => {
-        body[key] = val;
-    });
-    if (!statusMessage.textContent) {
-        statusMessage.textContent = loadMessage;
-    } else {
-        statusMessage.textContent = '';
-        statusMessage.textContent = loadMessage;
-    }
-    const input = form2.querySelectorAll('input');
-    for (let i = 0; i < input.length; i++) {
-        if (input[i].value !== '' && input[i].type !== 'radio') {
-            input[i].value = '';
-        }
-    }
-            const postData = body =>
-                fetch('./server.php', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(body)
-                });
+//     const formData = new FormData(form2);
+//     const body = {};
+//     formData.forEach((val, key) => {
+//         body[key] = val;
+//     });
+//     if (!statusMessage.textContent) {
+//         statusMessage.textContent = loadMessage;
+//     } else {
+//         statusMessage.textContent = '';
+//         statusMessage.textContent = loadMessage;
+//     }
+//     const input = form2.querySelectorAll('input');
+//     for (let i = 0; i < input.length; i++) {
+//         if (input[i].value !== '' && input[i].type !== 'radio') {
+//             input[i].value = '';
+//         }
+//     }
+//             const postData = body =>
+//                 fetch('./server.php', {
+//                     method: 'POST',
+//                     headers: {
+//                         'Content-Type': 'application/json'
+//                     },
+//                     body: JSON.stringify(body)
+//                 });
         
-            postData(body)
-                .then(response => {
-                    if (response.status !== 200) {
-                        throw new Error('status network not 200');
-                    }
-                    statusMessage.textContent = '';
-                    document.getElementById('thanks').style.display = 'block';
-                    document.getElementById('check1').checked = false;
+//             postData(body)
+//                 .then(response => {
+//                     if (response.status !== 200) {
+//                         throw new Error('status network not 200');
+//                     }
+//                     statusMessage.textContent = '';
+//                     document.getElementById('thanks').style.display = 'block';
+//                     document.getElementById('check1').checked = false;
 
-                    const deliteMessage = () => {
-                        if (statusMessage) {
-                            form2.removeChild(statusMessage);
-                        }
-                    };
-                    setTimeout(deliteMessage, 5000);
+//                     const deliteMessage = () => {
+//                         if (statusMessage) {
+//                             form2.removeChild(statusMessage);
+//                         }
+//                     };
+//                     setTimeout(deliteMessage, 5000);
                 
-                })
-                .catch(error => {
-                    statusMessage.textContent = errorMessage;
-                    document.getElementById('thanks').querySelector('h4').textContent = 'Извините';
-                    document.getElementById('thanks').querySelector('p').textContent = statusMessage.textContent;
-                    document.getElementById('thanks').style.display = 'block';
-                    document.getElementById('check1').checked = false;
-                });
-    // } else {
-        // statusMessage.textContent = 'Необходимо поставить галочку';
-        // form2.appendChild(statusMessage);
-        // const deliteMessage = () => {
-            // if (statusMessage) {
-                // form2.removeChild(statusMessage);
-            // }
-                // };
-                // setTimeout(deliteMessage, 3000);
-    // 
-    // }
+//                 })
+//                 .catch(error => {
+//                     statusMessage.textContent = errorMessage;
+//                     document.getElementById('thanks').querySelector('h4').textContent = 'Извините';
+//                     document.getElementById('thanks').querySelector('p').textContent = statusMessage.textContent;
+//                     document.getElementById('thanks').style.display = 'block';
+//                     document.getElementById('check1').checked = false;
+//                 });
+//     // } else {
+//         // statusMessage.textContent = 'Необходимо поставить галочку';
+//         // form2.appendChild(statusMessage);
+//         // const deliteMessage = () => {
+//             // if (statusMessage) {
+//                 // form2.removeChild(statusMessage);
+//             // }
+//                 // };
+//                 // setTimeout(deliteMessage, 3000);
+//     // 
+//     // }
     
         
-});
+// });
 
 
 };
--- src/modules/sendForms.js
+++ src/modules/sendForms.js
@@ -1,9 +1,11 @@
 const sendForms = () => {
 
 
-const form1 = document.getElementById('banner-form'),
+const bannerForm = document.getElementById('banner-form'),
 form2 = document.getElementById('form2'), 
-form3 = document.getElementById('form3');
+form1 = document.getElementById('form1'),
+footerForm = document.getElementById('footer_form'),
+cardOrder = document.getElementById('card_order');
 
 const errorMessage = 'Что то пошло не так...',
     loadMessage = 'Загрузка...',
@@ -12,41 +14,88 @@ const errorMessage = 'Что то пошло не так...',
 
 const statusMessage = document.createElement('div');
 statusMessage.classList.add('status-message');
-statusMessage.style.cssText = 'font-size: 2rem;';
+statusMessage.style.cssText = 
+`font-size: 18px;
+color: red;`;
 
 const send = form => {
+
     form.addEventListener('submit', event => {
     event.preventDefault();
-    if (document.getElementById('check1').checked === true){
+    for (let inp of form.querySelectorAll('input')){
+        if (inp.type === "checkbox"){
+            if (inp.checked) {
+                form.appendChild(statusMessage);
+            
+                const formData = new FormData(form);
+                const body = {};
+                formData.forEach((val, key) => {
+                    body[key] = val;
+                });
+                if (!statusMessage.textContent) {
+                    statusMessage.textContent = loadMessage;
+                } else {
+                    statusMessage.textContent = '';
+                    statusMessage.textContent = loadMessage;
+                }
+                const input = form.querySelectorAll('input'); 
 
-        form.appendChild(statusMessage);
-
-    const formData = new FormData(form);
-    const body = {};
-    formData.forEach((val, key) => {
-        body[key] = val;
-    });
-    if (!statusMessage.textContent) {
-        statusMessage.textContent = loadMessage;
-    } else {
-        statusMessage.textContent = '';
-        statusMessage.textContent = loadMessage;
-    }
-    const input = form.querySelectorAll('input');
-    for (let i = 0; i < input.length; i++) {
-        if (input[i].value !== '' && input[i].type !== 'radio') {
-            input[i].value = '';
-        }
-    }
-
-    const deliteMessage = () => {
-        if (statusMessage) {
-            statusMessage.textContent = '';
-            // form1.removeChild(statusMessage);
-        }
-    };
-            // setTimeout(deliteMessage, 2000);
+                const postData = body =>
+                    fetch('./server.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(body)
+                    });
+                    
+                postData(body)
+                    .then(response => {
+                        if (response.status !== 200) {
+                            throw new Error('status network not 200');
+                        }
+                        document.getElementById('thanks').style.display = 'block';
+                        statusMessage.textContent = '';
+                    })
+                    .catch(error => {
+                        statusMessage.textContent = errorMessage;
+                        document.getElementById('thanks').querySelector('h4').textContent = 'Извините';
+                        document.getElementById('thanks').querySelector('p').textContent = statusMessage.textContent;
+                        document.getElementById('thanks').style.display = 'block';
+                        statusMessage.textContent = '';
+                    });
+                    for (let i = 0; i < input.length; i++) {
+                        if (input[i].value !== '' && input[i].type !== 'radio' && input[i].type !== 'hidden') {
+                            input[i].value = '';
+                        }
+                    }
+                    inp.checked = false;
+            
+            } 
+            else {
+                statusMessage.textContent = 'Необходимо поставить галочку';
+                form.appendChild(statusMessage);
+            }
+        } else if (form === footerForm){
+            form.appendChild(statusMessage);
         
+            const formData = new FormData(form);
+            const body = {};
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+            if (!statusMessage.textContent) {
+                statusMessage.textContent = loadMessage;
+            } else {
+                statusMessage.textContent = '';
+                statusMessage.textContent = loadMessage;
+            }
+            const input = form.querySelectorAll('input');
+            for (let i = 0; i < input.length; i++) {
+                if (input[i].value !== '' && input[i].type !== 'radio' && input[i].type !== 'hidden') {
+                    input[i].value = '';
+                }
+            }        
             const postData = body =>
                 fetch('./server.php', {
                     method: 'POST',
@@ -55,60 +104,33 @@ const send = form => {
                     },
                     body: JSON.stringify(body)
                 });
-        
+
             postData(body)
                 .then(response => {
                     if (response.status !== 200) {
                         throw new Error('status network not 200');
                     }
-                    statusMessage.textContent = '';
                     document.getElementById('thanks').style.display = 'block';
-                    document.getElementById('check1').checked = false;
+                    statusMessage.textContent = '';
                 })
                 .catch(error => {
-                    statusMessage.textContent = errorMessage;
                     document.getElementById('thanks').querySelector('h4').textContent = 'Извините';
                     document.getElementById('thanks').querySelector('p').textContent = statusMessage.textContent;
                     document.getElementById('thanks').style.display = 'block';
-                    document.getElementById('check1').checked = false;
+                    statusMessage.textContent = '';
                 });
-    } else {
-        statusMessage.textContent = 'Необходимо поставить галочку';
-        form.appendChild(statusMessage);
-        const deliteMessage = () => {
-            if (statusMessage) {
-                statusMessage.textContent = '';
-                // form1.removeChild(statusMessage);
-            }
-                };
-                setTimeout(deliteMessage, 2000);
+        }
     }
-    
-        
 });
-
 }
 
 send(form1);
-send(form2)
-
-
-    
-    
-
-
-
-
-
-
-
-
-
+send(form2);
+send(bannerForm);
+send(footerForm);
+send(cardOrder)
 
 };
-
-
-
 
 
 
